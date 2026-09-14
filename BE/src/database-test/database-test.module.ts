12e371a4f71b5680b12d0251706ce9b223d48019
Quan Nguyen Huu <quan.nguyenhuu@ncc.asia>
2026-09-14 14:49:42 +0700

    update ssl

--- BE/src/database-test/database-test.module.ts
+++ BE/src/database-test/database-test.module.ts
@@ -25,7 +25,7 @@ import { Admin } from './entities/admin.entity';
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
-        ssl: configService.get<string>('DB_SSL') === 'true' ? { rejectUnauthorized: false } : false,
+        ssl: { rejectUnauthorized: false },
         entities: [
           User,
           CustomPrompt,
